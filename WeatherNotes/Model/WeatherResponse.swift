80a107ae538308c761da311bbc8769c963d582cf
Rodion Rubets <rodion.rubets@gmail.com>
2026-03-05 14:14:02 +0100

    Updating WeatherResponse for showing city

---
+++ WeatherNotes/Model/WeatherResponse.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct WeatherResponse: Codable {
+
+    let main: Main
+    let weather: [Weather]
+
+    struct Main: Codable {
+        let temp: Double
+    }
+
+    struct Weather: Codable {
+        let description: String
+        let icon: String
+        let name: String
+    }
+}
