82d212b7046f75bc33ae50c8177b645e7848b378
Rodion Rubets <rodion.rubets@gmail.com>
2026-03-05 13:13:17 +0100

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Rodion Rubets on 05.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
