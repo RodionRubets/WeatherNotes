c3283f2d40429c6f11dd0e2faddf5154198e344b
Rodion Rubets <rodion.rubets@gmail.com>
2026-03-05 14:17:27 +0100

    Finished NotesViewModel and added func addNote

--- WeatherNotes/ViewModels/NotesViewModel.swift
+++ WeatherNotes/ViewModels/NotesViewModel.swift
@@ -10,4 +10,40 @@ class NotesViewModel: ObservableObject {
     private let storage = StorageManager()
     private let locationManager = LocationManager()
 
+    
+    init() {
+        notes = storage.loadNotes()
+    }
+    
+    func addNote(text: String) {
+        locationManager.onLocationUpdate = { [weak self] location in
+            self?.weatherService.fetchWeather(
+                latitude: location.coordinate.latitude,
+                longitude: location.coordinate.longitude
+            ) { result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let weather):
+                        let note = Note(id: UUID(),
+                                        text: text,
+                                        date: Date(),
+                                        temperature: weather.main.temp,
+                                        description: weather.weather.first?.description ?? "",
+                                        icon: weather.weather.first?.icon ?? "",
+                                        locationName: weather.weather.first?.name ?? ""
+                        )
+                        
+                        self?.notes.append(note)
+                        self?.storage.saveNotes(notes: self?.notes ?? [])
+                        
+                    case .failure(let error):
+                        self?.errorMasage = error.localizedDescription
+                    }
+                }
+            }
+               
+        }
+        
+        locationManager.requestLocation()
+    }
 }
